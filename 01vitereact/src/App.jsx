fdad2f8c7cc1b24b1877f52658e0d35dd54247d0
Devanshrathod12 <rathoddevansh53@gmail.com>
2024-01-14 00:30:32 +0530

    projects start

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,9 +1,15 @@
 
+import Chai from "./chai"
 
 function App() {
 
+  const username = "chai  aur code"
   return (
-      <h1> Chai aur aur react with vite |DH</h1>
+     <>
+      <Chai/>
+      <h1>chai aur react {username} </h1>
+      <p>test para</p>
+     </>
   )
 }
 
